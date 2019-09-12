bfdac045b0e26519b91729ed63c25eeb86680491
DavidLuna0 <ddavidr569@gmail.com>
2019-09-12 09:49:09 -0300

    [CREATE]Project initial structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Navbar/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   }
 }
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="navbar">
+        <h1>Teste</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style>
+
+.navbar {
+    height: 60px;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    background-color: darkslategrey;
+}
+</style>
